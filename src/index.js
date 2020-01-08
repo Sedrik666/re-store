b1d578e80b14a85cbc3768ef62245cf915ba00d9
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-08 18:12:17 +0200

    feat(App): create structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello Word');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app'
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
