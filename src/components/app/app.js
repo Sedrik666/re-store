d1dc08b9cd8fc6cde4d82cb59ad062f752febb4a
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-09 16:24:25 +0200

    feat(App): frame react-redux application

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from "react";
+import withBookstoreService from '../hoc/with-bookstore-service';
 import './app.css'
 
-const App = () => {
+const App = ({ bookstoreService }) => {
+    console.log(bookstoreService.getBooks());
     return (
         <div>App</div>
     )
 };
 
-export default App;
+export default withBookstoreService()(App);
 
