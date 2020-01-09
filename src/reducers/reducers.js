8fdf6e7dc62f1fc3d02a319c696714a108d08e45
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-09 17:25:28 +0200

    feat(App): read redux store

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,16 @@
 const initialState = {
-    books: []
+    books: [
+        {
+            id: 1,
+            title: 'Production-Ready Microservices',
+            author: 'Susan J. Fowler'
+        },
+        {
+            id: 2,
+            title: 'Realise It!',
+            author: 'Michael T. Niggard'
+        }
+    ]
 };
 
 const reducer = (state = initialState, action) => {
