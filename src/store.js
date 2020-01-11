d7c48f42edb155a9e125e007592cfaef63a18d66
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-11 18:04:34 +0200

    learn(Enhancer): example use enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,37 @@
-import {createStore} from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from "./reducers/reducers";
 
-const store = createStore(reducer);
+const stringEnhancer  = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        if(typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
+        originalDispatch(action)
+    };
+
+    return store;
+};
+
+const logEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+        console.log(action.type);
+        originalDispatch(action)
+    };
+
+    return store;
+};
+
+const store = createStore(reducer, compose(stringEnhancer,logEnhancer));
+
+
+store.dispatch('HELLO_WORD');
 
 export default store;
