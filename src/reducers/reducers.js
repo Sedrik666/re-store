310a0d912d5f22ca884eb14ce73dbf565df3d4bf
V.Fedorenko <v.fedorenko@cosmonova.net>
2020-01-11 11:01:23 +0200

    feat(Redux): add to array

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,24 +2,12 @@ const initialState = {
     books: [],
     loading: true,
     error: null,
-    cartItems: [
-        {
-            id: 1,
-            name: 'Book 1',
-            count: 3,
-            total: 150
-        },
-        {
-            id: 2,
-            name: 'Book 2',
-            count: 2,
-            total: 170
-        },
-    ],
+    cartItems: [],
     orderTotal: 220
 };
 
 const reducer = (state = initialState, action) => {
+    console.log(action.type);
     switch (action.type) {
         case 'FETCH_BOOKS_REQUEST':
             return {
@@ -41,6 +29,37 @@ const reducer = (state = initialState, action) => {
                 books: [],
                 loading: false,
                 error: action.payload
+            };
+        case 'BOOK_ADDED_TO_CART':
+            const bookId = action.payload;
+            const book = state.books.find((book) => book.id === bookId);
+            //const existingBook = state.cartItems.find((book) => book.id === bookId);
+            let newItem = {};
+
+                newItem = {
+                    id: book.id,
+                    name: book.title,
+                    count: 1,
+                    total: book.price
+                };
+           /* if(existingBook===undefined){
+            }else{
+                newItem = {
+                    id: book.id,
+                    name: book.title,
+                    count: existingBook.count + 1,
+                    total: book.price * (existingBook.count + 1),
+                };
+            }*/
+
+
+            return {
+                ...state,
+                loading: false,
+                cartItems:[
+                    ...state.cartItems,
+                    newItem
+                ]
             };
         default: 
             return state;
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -4,14 +4,14 @@ export default class BookstoreService{
             id: 1,
             title: 'Production-Ready Microservices',
             author: 'Susan J. Fowler',
-            price: '$32',
+            price: '32',
             coverImage: 'https://www.mann-ivanov-ferber.ru/assets/images/covers/01/18401/1.00x-thumb.png',
         },
         {
             id: 2,
             title: 'Realise It!',
             author: 'Michael T. Niggard',
-            price: '$45',
+            price: '45',
             coverImage: 'https://www.mann-ivanov-ferber.ru/assets/images/covers/79/23279/1.00x-thumb.png',
         }
 
